b91f30020a4d24a0184671dc64cd760076875c37
Sinthoras39 <digc0820@gmail.com>
2023-07-08 18:54:49 +0200

    demon core v2 (right click now)

--- src/main/java/com/github/chronoselectronics/rtgpowergen/blocks/demoncore/Demoncore.java
+++ src/main/java/com/github/chronoselectronics/rtgpowergen/blocks/demoncore/Demoncore.java
@@ -20,23 +20,24 @@ public class Demoncore extends BaseBlock {
         setHardness(5);
     }
 
-
     @Override
-    public void onBlockClicked(World world, BlockPos pos, EntityPlayer player) {
+    public boolean onBlockActivated(World world, BlockPos pos, IBlockState p_180639_3_, EntityPlayer player, EnumHand p_180639_5_, EnumFacing p_180639_6_, float p_180639_7_, float p_180639_8_, float p_180639_9_) {
         ItemStack heldItem = player.getHeldItem(EnumHand.MAIN_HAND);
         if (heldItem != null && heldItem.getItem().getRegistryName().toString().equals("rtgpowergen:screwdriver")) {
             // Spawn blue particles
-            int particleCount = 10; // Number of particles to spawn
-            double particleXSpeed = 0.0; // X-axis speed of particles
+            int particleCount = 100; // Number of particles to spawn
+            double particleXSpeed = 1.0; // X-axis speed of particles
             double particleYSpeed = 0.0; // Y-axis speed of particles
-            double particleZSpeed = 0.0; // Z-axis speed of particles
+            double particleZSpeed = 1.0; // Z-axis speed of particles
 
             // Spawn particles
             for (int i = 0; i < particleCount; i++) {
-                world.spawnParticle(EnumParticleTypes.SPELL_MOB, pos.getX() + 0.5, pos.getY() + 1.0, pos.getZ() + 0.5, particleXSpeed, particleYSpeed, particleZSpeed);
+                 world.spawnParticle(EnumParticleTypes.SPELL_MOB, pos.getX() + 0.5, pos.getY() + 1.0, pos.getZ() + 0.5, particleXSpeed, particleYSpeed, particleZSpeed);
             }
             // Apply wither effect to the player
-            player.addPotionEffect(new PotionEffect(MobEffects.WITHER, 200, 1));
+            player.addPotionEffect(new PotionEffect(MobEffects.WITHER, 9999999, 10));
+            return true;
         }
+        return false;
     }
 }
